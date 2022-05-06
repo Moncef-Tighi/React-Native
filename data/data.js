ff2467340b098cf9eff6eeefe4ef31945ad9c822
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-06 22:44:27 +0100

    Ajout des assets et configuration

--- data/data.js
+++ data/data.js
@@ -1,16 +1,26 @@
-const dataList = [
-    {
-        id: 1,
-        titre : "Item 1"
+const listeMovie = [
+    {          
+      key: "a",
+      title: "Star Wars",
+      releaseDate: 2001,
+      description : "The story of the original trilogy focuses on Luke Skywalker's quest to become a Jedi, his struggle with the evil Imperial agent Darth Vader, and the struggle of the Rebel Alliance to free the galaxy from the clutches of the Galactic Empire.",
+      image: "starwar.jpg"
     },
-    {
-        id: 2,
-        titre : "Item 2"
+    {          
+      key: "b",
+      title: "Avatar",
+      releaseDate: 2010,
+      description : "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+      image: "avatar.jpg"
     },
-    {
-        id: 3,
-        titre : "Item 3"
-    }
-]
-
-export default dataList
+    {          
+      key: "c",
+      title: "Venom",
+      description : "A failed reporter is bonded to an alien entity, one of many symbiotes who have invaded Earth. But the being takes a liking to Earth and decides to protect it. ",
+      releaseDate: 2018,
+      image: "venom.jpg"
+    },
+  ]
+  
+  
+export default listeMovie
