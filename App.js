9c6a1df79dd6b8a69c8d829b554f5a76f26611bd
Moncef_Tighi <monceftighiouart@gmail.com>
2022-05-06 22:43:13 +0100

    Importation du projet movies sur React native

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 import ItemList from './components/ItemList';
+import React from 'react';
+import AddMovie from './components/addMovie';
+import MovieList from './components/Movie.list';
+import listeMovie from './data/data';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test edit</Text>
-      <Text>New Texte</Text>
-      <ItemList/>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.section}>
+          <AddMovie/>
+      </View>
+      <View style={styles.section}>
+        <Button style={styles.button}>Afficher les films</Button>
+      </View>
+      <View style={styles.section}>
+          <MovieList movies={listeMovie}/>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 15,
+    backgroundColor: "dimgray",
+    // justifyContent: "center",
+    // alignItems: "center"
   },
+  section: {
+    flex: 1,
+    margin: 5,
+    textAlign: "center",
+    backgroundColor: "white",
+    padding: 2,
+  },
+  
+  button: {
+    cursor: "pointer",
+    backgroundColor: "darkslateblue",
+    borderWidth: 1,
+    borderStyle: "solid",
+    color: "white",
+    padding: 0.75,
+    width: 50
+  }
 });
 
